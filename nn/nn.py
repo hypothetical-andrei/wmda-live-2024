c8251df9828e2f385ab9892e8fc957dccce3a98b
AT <hypothetical.andrei@gmail.com>
2024-05-08 19:09:23 +0300

    .

--- nn/nn.py
+++ nn/nn.py
@@ -3,42 +3,44 @@ import random
 
 class Network:
   def __init__(self, sizes):
+    self.num_layers = len(sizes)
     self.sizes = sizes
     self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
     self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-  def feed_forward(self, a):
+  def feedforward(self, a):
     for b, w in zip(self.biases, self.weights):
       a = sigmoid(np.dot(w, a) + b)
     return a
-  def back_prop(self, x, y):
+  def backprop(self, x, y):
     nabla_b = [np.zeros(b.shape) for b in self.biases]
     nabla_w = [np.zeros(w.shape) for w in self.weights]
     activation = x
-    activation = [x]
+    activations = [x]
     zs = []
     for b, w in zip(self.biases, self.weights):
-      z = np.dot(w, activation)
+      z = np.dot(w, activation) + b
       zs.append(z)
       activation = sigmoid(z)
-      activation.append(activation)
+      activations.append(activation)
     delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
-    nabla_b = delta
-    nabla_w = np.dot(delta, activations[-2].transpose())
+    nabla_b[-1] = delta
+    nabla_w[-1] = np.dot(delta, activations[-2].transpose())
     for l in range(2, self.num_layers):
       z = zs[-l]
       sp = sigmoid_prime(z)
-      delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+      delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
       nabla_b[-l] = delta
-      nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+      nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+    return (nabla_b, nabla_w)
   def update_mini_batch(self, mini_batch, eta):
     nabla_b = [np.zeros(b.shape) for b in self.biases]
     nabla_w = [np.zeros(w.shape) for w in self.weights]
     for x, y in mini_batch:
-      delta_b, delta_w = self.back_prop(x, y)
+      delta_b, delta_w = self.backprop(x, y)
       nabla_b = [nb + db for nb, db in zip(nabla_b, delta_b)]
       nabla_w = [nw + dw for nw, dw in zip(nabla_w, delta_w)]
-    self.weights = [w - (eta / len(mini_batch) * nw for w, nw in zip(self.weights, nabla_w))]
-    self.biases = [b - (eta / len(mini_batch) * nb for b, nb in zip(self.biases, nabla_b))]
+    self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+    self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
   def gradient_descent(self, training_data, epochs, mini_batch_size, eta, test_data = None):
     training_data = list(training_data)
     n = len(training_data)
@@ -53,9 +55,9 @@ class Network:
       if test_data:
         print('Epoch {} : {} / {}'.format(i, self.evaluate(test_data), n_test))
       else:
-        print('Epoch {}'.format(i))
+        print('Epoch {} finished'.format(j))
   def evaluate(self, test_data):
-    test_results = [(np.argmax(self.feed_forward(x)), y) for (x, y) in test_data]
+    test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
     return sum(int(a == y) for (a, y) in test_results)
   def cost_derivative(self, output_activations, y):
     return (output_activations - y)
